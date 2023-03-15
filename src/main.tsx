9b1ff325e3fde255bade5c125edf308c0a993083
Arek <arek.stachyra41@gmail.com>
2023-03-15 13:26:37 +0100

    feat: add initial routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Layout from './Layout';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+
+const router = createBrowserRouter([
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: '/products',
+				element: <Products />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
