69f13365d8481c519accade093890fa57c36f21e
Arek <arek.stachyra41@gmail.com>
2023-03-19 20:18:57 +0100

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer(): JSX.Element {
+	return (
+		<footer className="bg-slate-400 py-4 text-center">
+			Made with ❤️ by{' '}
+			<a href="https://github.com/Ver0s" target="_blank" rel="noreferrer">
+				Arek
+			</a>
+		</footer>
+	);
+}
