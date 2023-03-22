eb720e0e462a968f4164a1af562c561abd653cbb
Arek <arek.stachyra41@gmail.com>
2023-03-22 12:50:03 +0100

    feat: add products page

---
+++ src/pages/Products.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProductsList from '../components/ProductsList';
+
+export default function Products() {
+	return (
+		<>
+			<span>future category sorting here</span>
+			<ProductsList />
+		</>
+	);
+}
