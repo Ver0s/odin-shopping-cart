469061fd1c256ec5709a807985ed3a5de54f5666
Arek <arek.stachyra41@gmail.com>
2023-03-22 12:49:05 +0100

    feat: add product details component

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFakeStore from '../hooks/useFakeStore';
+import type { Product } from '../types';
+import { Link } from 'react-router-dom';
+
+export default function ProductDetails() {
+	const { productId } = useParams();
+	const { data, status } = useFakeStore<Product>(
+		`https://fakestoreapi.com/products/${productId}`,
+		null
+	);
+
+	if (status === 'loading') return <span>loading</span>;
+
+	if (status === 'error') {
+		return (
+			<span className="text-xl font-bold text-red-600">
+				Failed to fetch product details
+			</span>
+		);
+	}
+
+	return (
+		<div>
+			<Link to={'/products'}>
+				<div className="flex gap-1">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="icon icon-tabler icon-tabler-arrow-narrow-left"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						strokeWidth="2"
+						stroke="currentColor"
+						fill="none"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<path
+							stroke="none"
+							d="M0 0h24v24H0z"
+							fill="none"
+						></path>
+						<path d="M5 12l14 0"></path>
+						<path d="M5 12l4 4"></path>
+						<path d="M5 12l4 -4"></path>
+					</svg>
+					Back to products
+				</div>
+			</Link>
+			<br />
+			{data.title}
+		</div>
+	);
+}
