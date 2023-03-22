93ba17400a2e07bfe77d7cd2744ec22df84ab4d9
Arek <arek.stachyra41@gmail.com>
2023-03-22 12:46:27 +0100

    feat: expand layout components functionality

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Outlet, useOutletContext } from 'react-router-dom';
 import Header from './components/Header';
+import useLocalStorage from './hooks/useLocalStorage';
 // import Footer from './components/Footer';
 
 type CartType = {
@@ -9,17 +10,41 @@ type CartType = {
 };
 
 type ContextType = {
-	cart: CartType;
+	cart: CartType[];
 	setCart: React.Dispatch<React.SetStateAction<CartType[]>>;
+	handleAddToCart: (id: number) => void;
 };
 
 export default function Layout() {
-	const [cart, setCart] = useState<CartType[]>([]);
+	const [cart, setCart] = useLocalStorage<CartType[]>('cartProducts', []);
+	const totalProducts = cart.reduce(
+		(total, currentProduct) => total + currentProduct.quantity,
+		0
+	);
+
+	function handleAddToCart(id: number) {
+		let nextCart;
+		const isInCart = cart.some((product) => product.productId === id);
+
+		if (isInCart) {
+			nextCart = cart.map((product) => {
+				if (product.productId === id) {
+					return { ...product, quantity: product.quantity + 1 };
+				} else {
+					return product;
+				}
+			});
+		} else {
+			nextCart = [...cart, { productId: id, quantity: 1 }];
+		}
+
+		setCart(nextCart);
+	}
 
 	return (
 		<>
-			<Header />
-			<Outlet context={{ cart, setCart }} />
+			<Header totalProducts={totalProducts} />
+			<Outlet context={{ cart, setCart, handleAddToCart }} />
 			{/* <Footer /> */}
 		</>
 	);
