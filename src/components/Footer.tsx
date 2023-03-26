9e29333d3b3d48c9f1c7975e1360e3c1528ee76e
Arek <arek.stachyra41@gmail.com>
2023-03-26 19:50:03 +0200

    feat: adjust footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer(): JSX.Element {
 	return (
-		<footer className="bg-slate-400 py-4 text-center">
+		<footer className="mx-auto w-full max-w-screen-xl border-t border-gray-200 py-4 text-center">
 			Made with ❤️ by{' '}
 			<a href="https://github.com/Ver0s" target="_blank" rel="noreferrer">
 				Arek
