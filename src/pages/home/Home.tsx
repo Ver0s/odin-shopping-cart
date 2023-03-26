85b5e002116803275d1b348e7a26c83e8cf84264
Arek <arek.stachyra41@gmail.com>
2023-03-26 19:47:19 +0200

    feat: style homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import homeImg from '../../assets/homeImg.webp';
 
 export default function Home(): JSX.Element {
 	return (
-		<>
-			<h1>The one stop shop for all your needs</h1>
-			<p>
-				Looking for some clothes, or maybe a new monitor? We&apos;ve got
-				you covered, <span className="font-bold">everything</span> is
-				here.
-			</p>
+		<div className="mx-auto flex h-full max-w-screen-xl items-center justify-between px-4 xl:px-0">
+			<div className="flex max-w-2xl flex-col gap-4">
+				<h1 className="text-6xl font-bold">
+					The one stop shop for all your needs
+				</h1>
+				<p className="text-xl">
+					Looking for some clothes, or maybe a new monitor? We&apos;ve
+					got you covered,{' '}
+					<span className="font-bold text-orange-500">
+						everything
+					</span>{' '}
+					is here.
+				</p>
+				<Link
+					to={'/products'}
+					className="inline-block self-start rounded-lg bg-orange-500 py-3 px-6 text-center font-bold text-white"
+				>
+					Shop now
+				</Link>
+			</div>
 			<img src={homeImg} className="hidden max-w-lg lg:block" />
+		</div>
 	);
 }
