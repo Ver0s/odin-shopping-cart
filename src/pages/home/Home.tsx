c93d0925330ac31560acc0184a484b5216f0ebe1
Arek <arek.stachyra41@gmail.com>
2023-03-27 16:58:45 +0200

    refactor: update main color

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,20 +6,20 @@ export default function Home(): JSX.Element {
 	return (
 		<div className="mx-auto flex h-full max-w-screen-xl items-center justify-between px-4 xl:px-0">
 			<div className="flex max-w-2xl flex-col gap-4">
-				<h1 className="text-6xl font-bold">
+				<h1 className="text-5xl font-bold sm:text-6xl">
 					The one stop shop for all your needs
 				</h1>
 				<p className="text-xl">
 					Looking for some clothes, or maybe a new monitor? We&apos;ve
 					got you covered,{' '}
-					<span className="font-bold text-orange-500">
+					<span className="font-bold text-indigo-700">
 						everything
 					</span>{' '}
 					is here.
 				</p>
 				<Link
 					to={'/products'}
-					className="inline-block self-start rounded-lg bg-orange-500 py-3 px-6 text-center font-bold text-white"
+					className="inline-block self-start rounded-lg bg-indigo-700 py-3 px-6 text-center font-bold text-white"
 				>
 					Shop now
 				</Link>
