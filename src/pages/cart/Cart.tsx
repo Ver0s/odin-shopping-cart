ae8f0be489a2af85c0377dd9bfa9afceb131a33f
Arek <arek.stachyra41@gmail.com>
2023-03-27 16:56:51 +0200

    feat: update cart structure

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -6,10 +6,6 @@ import CartProduct from './CartProduct';
 import { Link } from 'react-router-dom';
 import Spinner from '../../components/Spinner';
 
-// TODO:
-// Add divider to each item as a border at the bottom
-// Add total price of items and checkout link
-
 export default function Cart() {
 	const { data, isLoading, error } = useFakeStore<Product[]>(
 		'https://fakestoreapi.com/products'
@@ -50,25 +46,68 @@ export default function Cart() {
 			total + currentProduct.price * currentProduct.quantity,
 		0
 	);
+
 	return (
-		<div className="px-4 py-4 xl:px-0">
-			<div className="mb-4 flex items-center justify-between">
-				<h1 className="text-xl font-bold">Your cart</h1>
-				<button onClick={handleClearCart}>Clear Cart</button>
+		<div className="mx-auto flex h-full max-w-6xl flex-col gap-4 px-4 py-4 md:flex-row md:justify-between xl:gap-28 xl:px-0 xl:py-8">
+			<div className="md:flex-[2]">
+				<div className="mb-4 flex items-center justify-between">
+					<h1 className="text-xl font-bold">Your cart</h1>
+					<button
+						onClick={handleClearCart}
+						className="flex items-center gap-2 rounded-lg p-2 transition-colors hover:bg-gray-100"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							strokeWidth="1"
+							stroke="currentColor"
+							fill="none"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						>
+							<path
+								stroke="none"
+								d="M0 0h24v24H0z"
+								fill="none"
+							></path>
+							<path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+							<path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+							<path d="M17 17h-11v-14h-2"></path>
+							<path d="M6 5l8 .571m5.43 4.43l-.429 3h-13"></path>
+							<path d="M17 3l4 4"></path>
+							<path d="M21 3l-4 4"></path>
+						</svg>
+						Clear Cart
+					</button>
+				</div>
+				<ul className="space-y-4">
+					{cartProducts.map((product) => (
+						<li key={product.id}>
+							<CartProduct
+								id={product.id}
+								title={product.title}
+								price={product.price}
+								image={product.image}
+								initialQuantity={product.quantity}
+							/>
+						</li>
+					))}
+				</ul>
 			</div>
-			<ul className="space-y-4">
-				{cartProducts.map((product) => (
-					<li key={product.id}>
-						<CartProduct
-							id={product.id}
-							title={product.title}
-							price={product.price}
-							image={product.image}
-							initialQuantity={product.quantity}
-						/>
-					</li>
-				))}
-			</ul>
+			<div className="md:sticky md:top-[97px] md:flex-1 md:self-start xl:top-[123px]">
+				<div className="flex items-center justify-between text-lg font-bold">
+					<h2>Total:</h2>
+					<span>{totalPrice.toFixed(2)}$</span>
+				</div>
+				<Link
+					to="#"
+					className="mt-4 block rounded-lg bg-indigo-700 py-3 text-center font-bold text-white"
+				>
+					Checkout
+				</Link>
+			</div>
 		</div>
 	);
 }
